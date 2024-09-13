299cae10f98bd8f9b22af2d52277279e5e557649
Roman <gabbasov.tomsk@gmail.com>
2024-09-13 13:47:00 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ModalsContainer, useModal } from 'vue-final-modal'
+import ModalConfirm from '@/components/ModalConfirm.vue'
+const { open, close } = useModal({
+  component: ModalConfirm,
+  attrs: {
+    title: 'Заполните заявку, чтобы стать резидентом',
+    onConfirm() {
+      close()
+    },
+  },
+  slots: {
+    default: '<p>UseModal: The content of the modal</p>',
+  },
+})
+</script>
+
+<template>
+  <VButton @click="() => open()">
+    <button type="button"
+      class="px-5 py-2.5 text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm">
+      Open Modal</button>
+
+  </VButton>
+  <ModalsContainer />
+</template>
+
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+
+import { createApp } from 'vue'
+import { createVfm } from 'vue-final-modal'
+import 'vue-final-modal/style.css'
+import "@/index.css"
+import App from './App.vue'
+
+const app = createApp(App)
+
+const vfm = createVfm()
+app.use(vfm).mount('#app')
